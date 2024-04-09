d774cac7d16cc4099ccbb81920208fb65a800611
DucMinh <macleducminh1@gmail.com>
2024-04-09 16:37:06 +0700

    [demo thu cai heroku]

--- NewProject/app.py
+++ NewProject/app.py
@@ -1,4 +1,13 @@
 import pyodbc
+import cgi
+# form = cgi.FieldStorage()
+# username = form.getvalue('user')
+# email = form.getvalue('email')
+# password = form.getvalue('pass')
+
+# print(username)
+# print(email)
+# print(password)
 
 server = 'MSI'
 database = 'MHUD'
@@ -6,7 +15,23 @@ connection_string = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER=MSI;DATABASE=M
 
 try:
     conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+
+    # Xử lý dữ liệu từ form đăng kí
+    form = cgi.FieldStorage()
+    username = form.getvalue('user')
+    email = form.getvalue('email')
+    password = form.getvalue('pass')
+
+    print(username)
+    print(email)
+    print(password)
+    
+    cursor.execute('INSERT INTO users (username, email, password) VALUES (?, ?)', (username, email, password))
+    conn.commit()
     print("Ket noi thanh cong!")
     conn.close()
+    print("Location: Userpage.html\r\n\r\n")
 except Exception as e:
-    print("Loi khi ket noi den may chu SQL Server:", str(e).encode('ascii', 'ignore').decode('ascii'))
+    print("Loi khi ket noi server!", str(e))
+
