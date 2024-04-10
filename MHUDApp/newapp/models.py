9708e3b888e2461dc909c8a35f2cd3977c4fe338
DucMinh <macleducminh1@gmail.com>
2024-04-10 17:06:03 +0700

    xu li thong tin dang ky

--- MHUDApp/newapp/models.py
+++ MHUDApp/newapp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from typing import Any, Dict
 
 # Create your models here.
 class AddUser(models.Model):
@@ -7,6 +11,18 @@ class AddUser(models.Model):
     Password = models.CharField(max_length=100,null=False,blank=False)
     # ConfirmPassword = models.CharField(max_length=100,null=False,blank=False)
     
-
     def __str__(self) -> str:
         return self.username 
+    
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Re-enter password"}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean(self) -> Dict[str, Any]:
+        return super().clean()
--- MHUDApp/newapp/views.py
+++ MHUDApp/newapp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, HttpResponse
 from .models import AddUser
 from django.contrib.auth.models import User
 from django.contrib import messages
+import re
+
+import tkinter as tk
+from tkinter import messagebox
 
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
@@ -28,13 +32,71 @@ def Register(request):
         username = request.POST['user1']
         email = request.POST['email1']
         password = request.POST['pass1']
-        # confirmpassword = request.POST['cpass1']
+        confirmpassword = request.POST['cpass1']
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        if(is_valid_username(username) == True and is_valid_email(email) == True):
+            if(is_valid_password(password, confirmpassword) == True):
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+            else:
+                message = is_valid_password(password, confirmpassword)
+                messagebox.showerror(message)
+        else:
+            message = is_valid_username(username)
+            if message is not True:
+                messagebox.showerror(message)
+            else:
+                messagebox.showerror('Email is invalid')
         
-        user.save()
 
     return render(request, "RegisterPage.html")
+
+def is_valid_username(username):
+    # Kiểm tra chiều dài của tên người dùng
+    if len(username) < 5 or len(username) > 20:
+        return False
+    
+    # Kiểm tra các ký tự đặc biệt không được phép trong tên người dùng
+    special_characters = "!@#$%^&*()-_+=[]{}|\\;:'\"<>,.?/~"
+
+    all_users = User.objects.all()
+
+    # for user in all_users:
+    #     print(user.username)
+
+    for user in all_users:
+        if username == user.username:
+            return 'Username existed! Please choose other name'
+        else:
+            continue
+    
+    if any(char in special_characters for char in username):
+        return 'Username cannot include special character!'
+    
+    # Kiểm tra xem tên người dùng chỉ chứa các ký tự chữ cái, số và dấu gạch dưới
+    if not username.isalnum():
+        return 'Username cannot include all character or number!'
+    
+    # Nếu không có vấn đề gì, tên người dùng được coi là hợp lệ
+    return True
+
+def is_valid_email(email):
+    # Biểu thức chính quy để kiểm tra địa chỉ email
+    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    
+    # Kiểm tra địa chỉ email với biểu thức chính quy
+    if re.match(email_regex, email):
+        return True
+    else:
+        return False
+
+def is_valid_password(password1, password2):
+    if password1.isdigit():
+        return 'Password can not be all numbers!'
+    if password1 != password2:
+        return 'Password mismatch!'
+    
+    return True
 
 def User1(request):
     return render(request, "UserPage.html")
