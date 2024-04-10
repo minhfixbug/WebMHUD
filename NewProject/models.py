7187d09d0e80d39095161fcf326a3a6f23be0126
Nguyễn Thành Thuận <ntthuan@MacBook-Pro-cua-Nguyen.local>
2024-04-10 23:05:17 +0700

    [delete file]

--- NewProject/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class AddUsers(models.Model):
-    username = models.CharField(max_length=100, null=False,blank=False)
-    password = models.CharField(max_length=100, null=False,blank=False)
-    email = models.CharField(max_length=100, null=False,blank=False)
-    description = models
-
